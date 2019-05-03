93cde9d1c29cadeb18c47782ac8241f72eb1220c
Alejandro Paredes Alva <aparedes@aparedes.net>
2019-05-03 20:46:19 +0200

    cleanup

--- src/store/reducers/housesReducer.test.js
+++ src/store/reducers/housesReducer.test.js
@@ -1,53 +1,55 @@
-import * as Immutable from 'immutable'
-import reducer from './housesReducer'
-
+import { List, Map, Set, is } from 'immutable';
+import reducer from './housesReducer';
 
 describe('Test House Reducer', () => {
-
   function expectIs(left, right) {
-    const comparison = Immutable.is(left, right);
+    const comparison = is(left, right);
     expect(comparison).toBe(true);
-    const commutative = Immutable.is(right, left);
+    const commutative = is(right, left);
     expect(commutative).toBe(true);
   }
 
   it('Returns Map from no action', () => {
     // Empty Reducer (This doesnt happen, usually it is given a REDUX action to start)
-    expectIs(reducer(), new Immutable.Map())
-  })
+    expectIs(reducer(), new Map());
+  });
 
-  let map = new Immutable.Map()
+  let map = new Map();
   it('Returns Map from unknown action', () => {
-    let action = { type: 'INIT_REDUX' }
-    expectIs(reducer(map, action), new Immutable.Map())
-  })
+    let action = { type: 'INIT_REDUX' };
+    expectIs(reducer(map, action), new Map());
+  });
 
   // let map = new Immutable.Map()
   it('GET_ALL_HOUSES', () => {
-    let action = { type: 'GET_ALL_HOUSES' }
-    expectIs(reducer(map, action), new Immutable.Map({ getting_all: true }))
-  })
+    let action = { type: 'GET_ALL_HOUSES' };
+    expectIs(reducer(map, action), new Map({ getting_all: true }));
+  });
 
   it('GET_ALL_HOUSES_ERROR after GET_ALL_HOUSES', () => {
-    let action = { type: 'GET_ALL_HOUSES_ERROR', error: 'Fetch Error' }
-    map = new Immutable.Map({ getting_all: true })
-    expectIs(reducer(map, action), new Immutable.Map({ error_all: 'Fetch Error' }))
-  })
+    let action = { type: 'GET_ALL_HOUSES_ERROR', error: 'Fetch Error' };
+    map = new Map({ getting_all: true });
+    expectIs(reducer(map, action), new Map({ error_all: 'Fetch Error' }));
+  });
 
   it('GET_ALL_HOUSES after GET_ALL_HOUSES', () => {
     let action = {
       type: 'GOT_ALL_HOUSES',
-      allHouses: new Immutable.Map({ '1': new Immutable.Map({ name: 'House Stark of Winterfell' })}),
-      allHousesIds: new Immutable.List([ '1' ]),
-      regions: new Immutable.Set([ 'The North' ]),
-    }
-    expectIs(reducer(map, action), new Immutable.Map({
-      houses: new Immutable.Map({ '1': new Immutable.Map({ name: 'House Stark of Winterfell' })}),
-      housesIds: new Immutable.List([ '1' ]),
-      regions: new Immutable.Set([ 'The North' ]),
-    }))
-  })
-
-
-
-})
+      allHouses: new Map({
+        '1': new Map({ name: 'House Stark of Winterfell' }),
+      }),
+      allHousesIds: new List(['1']),
+      regions: new Set(['The North']),
+    };
+    expectIs(
+      reducer(map, action),
+      new Map({
+        houses: new Map({
+          '1': new Map({ name: 'House Stark of Winterfell' }),
+        }),
+        housesIds: new List(['1']),
+        regions: new Set(['The North']),
+      })
+    );
+  });
+});
